f7f7030592c95304006c327441456b4c44528a29
MD. Bazrul Haque Sobuj <md.bazrulhaquesobuj1@gmail.com>
2025-06-02 01:15:19 +0600

    app settings done

--- lib/Spash_Screen_01.dart
+++ lib/Spash_Screen_01.dart
@@ -10,8 +10,19 @@ class SpashScreen01 extends StatefulWidget {
 class _SpashScreen01State extends State<SpashScreen01> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Money Note"),
+        backgroundColor: const Color.fromARGB(255, 167, 254, 206)
+      ),
+      body: 
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+Text("This is my won apk",style: TextStyle(fontSize: 30,fontWeight: FontWeight.w700),)
+        ],
+      ),
     );
   }
 }
